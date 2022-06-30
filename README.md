40291c1b1f06af9ceccd49adccee84b96d820289
woo <wooomr2@gmail.com>
2022-06-30 13:49:08 +0900

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Deploy 주소
 https://amazon-wooomr2.vercel.app
 
-# Next.js tailwind-css stripe stripeCLI
+# Next.js tailwind-css stripe 
+
+# stripeCLI
+- ./stripe login
+- stripe listen --forward-to localhost:3000/api/webhook
--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -2,10 +2,22 @@ import * as admin from "firebase-admin";
 import { buffer } from "micro";
 
 //Secure a connection to firebase from backend
-const {firebasePermission} = require("../../../permission.json");
 const app = !admin.apps.length
   ? admin.initializeApp({
-      credential: admin.credential.cert(firebasePermission),
+      credential: admin.credential.cert(
+        {
+          "type": "service_account",
+          "project_id": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+          "private_key_id": process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+          "private_key": process.env.NEXT_PUBLIC_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          "client_email": process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+          "client_id": process.env.NEXT_PUBLIC_CLIENT_ID,
+          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+          "token_uri": "https://oauth2.googleapis.com/token",
+          "auth_provider_x509_cert_url": process.env.NEXT_PUBLIC_AUTH_PROVIER_X509_CERT_URL,
+          "client_x509_cert_url": process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL,
+        }
+      ),
     })
   : admin.app();
 
@@ -44,7 +56,6 @@ export default async (req, res) => {
     //Verify that the EVENT posted came from stripe
     try {
       event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-
     } catch (error) {
       return res.status(400).send(`Weebhook error : ${error.message}`);
     }
